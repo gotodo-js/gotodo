a1adb0fe4f6c0fbaf6d4fd50f792badc8101f131
Yoki Yu <yokiyuqian@gmail.com>
2021-06-09 18:01:20 +0800

    feat: cli

--- packages/cli/bin/gotodo.js
+++ packages/cli/bin/gotodo.js
@@ -2,5 +2,19 @@
 
 const { prompt } = require('enquirer')
 const yargs = require('yargs')
+const { hideBin } = require('yargs/helpers')
 
-yargs.usage
+/**
+ * Design:
+ Usage:
+gotodo
+-  add <something>		‘add’ one todo’
+-  done <something>		‘make one todo done’ —all ‘make all todos doen’
+-  remove <something> 		 ‘remove one todo’  —all  ‘remove all todos’
+-  clear		‘clear all removed todos permanently’
+-  start  ‘start gotodo server’  (default to web) —desktop ‘start desktop app’
+ */
+
+yargs(hideBin(process.argv)).usage(
+  `$0 \n\n 💘 The best todolist tool for developers.`
+).argv
